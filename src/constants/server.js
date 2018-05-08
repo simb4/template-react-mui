4c8b3754443858226eb1d2b0274d6d3a41ae466d
Alimzhan <alimzhan.amanov@gmail.com>
2018-05-08 20:36:43 +0600

    build

--- src/constants/server.js
+++ src/constants/server.js
@@ -2,6 +2,6 @@
 // export const SERVER_URL = "http://18.195.201.196/api/"
 export const SERVER_URL = process.env.REACT_APP_ENV === 'prod'
 	? "https://api.1fit.kz/api/"
-	: "http://89.219.32.28/api/";
+	: "https://api.1fit.kz/api/";
 
 export const SITE_URL = "https://1fit.kz";
